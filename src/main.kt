ca3dcfc636e9b99b929a1452b3fa1f5fe1d26429
ste-xx <stefan.breitenstein@gmail.com>
2018-03-11 10:08:15 +0100

    use when statement

--- src/main.kt
+++ src/main.kt
@@ -6,37 +6,39 @@ public fun sort(list: List<Int>): List<Int> {
     if (list.size <= 1) {
         return list
     }
-    val left = list.subList(0,list.size/2)
-    val right = if(list.size/2 > 0) list.subList((list.size/2),list.size) else listOf()
+    val left = list.subList(0, list.size / 2)
+    val right = if (list.size / 2 > 0) list.subList((list.size / 2), list.size) else listOf()
 
-    return merge(sort(left).toMutableList(),  sort(right).toMutableList())
+    return merge(sort(left).toMutableList(), sort(right).toMutableList())
 }
 
 
-private fun merge(left:MutableList<Int>, right:MutableList<Int>):List<Int>{
+private fun merge(left: MutableList<Int>, right: MutableList<Int>): List<Int> {
 
     val result = mutableListOf<Int>()
 
-    while(left.isNotEmpty() and right.isNotEmpty()){
+    while (left.isNotEmpty() or right.isNotEmpty()) {
+        when {
+            left.isNotEmpty() and right.isNotEmpty() -> {
+                if (left[0] <= right[0]) {
+                    result += left[0]
+                    left -= left[0]
+                } else {
+                    result += right[0]
+                    right -= right[0]
+                }
+            }
 
-        if(left[0] <= right[0]){
-            result += left[0]
-            left -= left[0]
-        }else{
-            result += right[0]
-            right -= right[0]
+            left.isNotEmpty() -> {
+                result += left[0]
+                left -= left[0]
+            }
+
+            right.isNotEmpty() -> {
+                result += right[0]
+                right -= right[0]
+            }
         }
-    }
-
-    while(left.isNotEmpty()){
-        result += left[0]
-        left -= left[0]
-    }
-
-
-    while(right.isNotEmpty()){
-        result += right[0]
-        right -= right[0]
     }
 
 
