2967104f96fb723e41dddfdea58ee50fd0361a02
ste-xx <stefan.breitenstein@gmail.com>
2018-03-11 14:09:34 +0100

    cleanup

--- src/main2.kt
+++ src/main2.kt
@@ -2,25 +2,24 @@ fun sort2(list: MutableList<Int>): List<Int> {
     if (list.size <= 1) {
         return list
     }
-    msort(list, 0, list.size - 1);
-    return list
+    return msort(list, 0, list.size - 1);
 }
 
-private fun msort(list: MutableList<Int>, startIdx: Int, endIdx: Int) {
+private fun msort(list: MutableList<Int>, startIdx: Int, endIdx: Int):List<Int>{
     //System.out.println("start: ${startIdx} end: ${endIdx} diff: ${endIdx - startIdx + 1}")
 
     if (endIdx - startIdx + 1 == 1) {
-        return
+        return list
     }
     val middleIdx = Math.floor((startIdx + endIdx) / 2.0).toInt();//(startIdx + endIdx)/2
     msort(list, startIdx, middleIdx);
     msort(list, middleIdx + 1, endIdx)
 
-
     val left = list.subList(startIdx, middleIdx + 1).toList()
     val right = list.subList(middleIdx + 1, endIdx + 1).toList()
 
     merge(list, startIdx..endIdx, left, right)
+    return list
 }
 
 private fun merge(list: MutableList<Int>, range:IntRange, left: List<Int>, right: List<Int>) {
